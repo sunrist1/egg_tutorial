7ec1521db2ecb0fdf55863ef3b8e95fabccdbf26
sunrist1 <294333575@qq.com>
2018-02-01 22:51:04 +0800

    a

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,7 +2,15 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
     async index() {
+        console.log(this.ctx.app.cities);
         this.ctx.body = '<a href="/news">hello world</a>';
+    }
+    async add(){
+        const ctx = this.ctx;
+        let count = ctx.cookies.get('count');
+        count = count ? Number(count) : 0;
+        ctx.cookies.set('count',++count);
+        ctx.body = count;
     }
 }
 
--- app/router.js
+++ app/router.js
@@ -2,5 +2,6 @@ module.exports = app => {
     const { router, controller } = app;
     const gzip = app.middlewares.gzip({threshold:1024});
     router.get('/', controller.home.index);
+    router.get('/add', controller.home.add);
     router.get('/news',gzip, controller.news.list);
 };
